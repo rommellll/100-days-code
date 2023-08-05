7f34172ae3ac0926f9e473cf748da39a5bbee7b7
rommellll <rommel@storekeeper.nl>
2023-08-05 10:08:32 +0800

    day 3 exercises

---
+++ day-4-randomisation-and-list/random-module.py
@@ -0,0 +1,13 @@
+import random
+
+#print random number betwen 1 and 10
+randomInteger = random.randint(1, 10)
+print(randomInteger)
+
+#print a floating number between 0 and 1
+randomFloat = random.random()
+print(randomFloat)
+
+#floating number to a higher number, 0 and 5
+randomFloat = random.random() * 5
+print(randomFloat)
