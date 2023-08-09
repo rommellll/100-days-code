85313571ea8fcc632141e4c68147dfeb3b6f0540
rommellll <rommel@storekeeper.nl>
2023-08-09 10:51:53 +0800

    updated

--- day-6-functions-and-karel/reeborg-maze.py
+++ day-6-functions-and-karel/reeborg-maze.py
@@ -1,12 +1,16 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
-
+while front_is_clear():
+    move()
+turn_left()        
 while not at_goal():
-    if front_is_clear():
+    if right_is_clear():
+        turn_right()
         move()
-        if right_is_clear():
-            turn_right()
+    elif front_is_clear():
+        move()
     else:
         turn_left()
