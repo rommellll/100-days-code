e1bfe67945e443163440cf674f9177c2ee6e44ed
rommellll <rommel@storekeeper.nl>
2023-08-16 11:43:07 +0800

    Added day 17 project - quiz game using oop

---
+++ day-17-creating-oop-quizzer/main.py
@@ -0,0 +1,19 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+from data import question_data
+
+question_bank = []
+
+for question in question_data:
+    q_text = question["question"]
+    q_answer = question["correct_answer"]
+    new_question = Question(q_text, q_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
